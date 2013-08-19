d0702f5a68f53f760da5d9dba79c5b31f66bbbc5
digsim <andreas.ruppen+github@gmail.com>
2013-08-19 16:45:18 +0200

    Code Refactoring. Went away from a big monolithic design

--- src/missing_tvshows.py
+++ src/missing_tvshows.py
@@ -57,37 +57,50 @@ class TVShows:
         self.__database = config.get("Config", "db")
         self.__cwd = os.getcwd()
         self.__log.debug('Database '+self.__database)
-        self.__log.debug("\033[1;33m"+self.__cwd+"\033[0m")
                             
-    def main(self):
+                            
+    def make_sql_queries(self):
+        con = sqlite3.connect(self.__database)
+        cur = con.cursor()
+         # Select TV-Shows where no episode has been watched
+        cur.execute('select * from (select tvshow.c00 as Title, episodeview.c12 as Season, count(*) as Episodes, tvshow.c12 as SeriesiD, episodeview.idSeason as SeasoniD, max(episodeview.playCount) as Played from episodeview join seasons on seasons.idSeason = episodeview.idSeason join tvshow on tvshow.idShow = seasons.idShow group by tvshow.c00, episodeview.c12 order by tvshow.c00) where Played is NULL;')
+        nonewatched = cur.fetchall()
+        
+        # Select TV-Shows where at least one Episode was played
+        cur.execute('select * from (select tvshow.c00 as Title, episodeview.c12 as Season, count(*) as Episodes, tvshow.c12 as SeriesiD, episodeview.idSeason as SeasoniD, sum(episodeview.playCount) as Played from episodeview join seasons on seasons.idSeason = episodeview.idSeason join tvshow on tvshow.idShow = seasons.idShow group by tvshow.c00, episodeview.c12 order by tvshow.c00) where Played is not NULL;')
+        somewatched = cur.fetchall()
+        con.close()
+        
+        return nonewatched,  somewatched
+        
+    def getTotalNumberOfEpisodes(self,  series_id,  season):
+        show = self.__db.get(series_id, "en" )
+        number_of_episodes = len(show[season])
+        return number_of_episodes
+   
+        
+    def getSeriesInformation(self):
         """The main function"""
-        self.__log.debug("input File is: "+self.__database)
+        nonewatched,  somewatched = self.make_sql_queries()
         con = sqlite3.connect(self.__database)
         cur = con.cursor()
         
-        print('##############################################################')
-        print('###################### Unwatched Missing #####################')
-        print('##############################################################')
-        # Select TV-Shows where no episode has been watched
-        cur.execute('select * from (select tvshow.c00 as Title, episodeview.c12 as Season, count(*) as Episodes, tvshow.c12 as SeriesiD, episodeview.idSeason as SeasoniD, max(episodeview.playCount) as Played from episodeview join seasons on seasons.idSeason = episodeview.idSeason join tvshow on tvshow.idShow = seasons.idShow group by tvshow.c00, episodeview.c12 order by tvshow.c00) where Played is NULL;')
-        rows = cur.fetchall()
-        finished_tvshows = []
+        unwatched_finished_shows = []
+        unwatched_unfinished_shows =  []
+        watchedsome_unfinished_shows = []
+        watchedsome_finished_shows = []
         
-        print('-------------------------------------------------------------------------------------------------------------------------------------------------')
-        print('|{:44s} | {:s} ({:s}/{:s})| {:74s}|'.format('Title', 'Season', 'Downloaded',  'Available',  'Missing'))
-        print('-------------------------------------------------------------------------------------------------------------------------------------------------')
-        for row in rows:
-            self.__log.debug('#####################################')
-            self.__log.debug('Currently Treating {:s}'.format(row[0]))
-            self.__log.debug('#####################################')
+
+        for row in nonewatched:
             if(int(row[1]) == 0): # Don't take into consideration Season 0
                 continue
-            show = self.__db.get(int(row[3]), "en" )
-            number_of_episodes = len(show[int(row[1])])
+            self.__log.debug('Currently treating series {:s} with id: {:s} and Season {:s}'.format(row[0],  row[3],  row[1]))
+            number_of_episodes = self.getTotalNumberOfEpisodes(int(row[3]),  int(row[1]))
             full_episodes = range(1, number_of_episodes+1)
             self.__log.debug('{:35s}: Season {:2s} and has {:2d}/{:2d} Episodes'.format( row[0],  row[1],  row[2],  number_of_episodes))
             
             if(int(number_of_episodes) != int(row[2])): # If number of local Episodes is different from TheTVDB
+                # Select all availalbe Episodes of current Series and Season
                 cur.execute('select tvshow.c00 as Title, episodeview.c12 as Season, episodeview.c13 as Episode, tvshow.c12 as SeriesiD  from episodeview join seasons on seasons.idSeason = episodeview.idSeason join tvshow on tvshow.idShow = seasons.idShow where Season={:s} and SeriesiD={:s}  order by tvshow.c00, episodeview.c12, episodeview.c13;'.format(row[1],  row[3]))
                 episodes = cur.fetchall()
                 present_episodes = []
@@ -96,31 +109,18 @@ class TVShows:
                 present_episodes = map(int,  present_episodes)
                 self.__log.debug('Present episodes '+str(present_episodes))
                 missing_episodes = list(set(full_episodes) - set(present_episodes))
-                self.__log.debug(str(missing_episodes)[1:-1])
-                print('|{:43s}: | S{:2s} ({:2d}/{:2d})| missing: {:74s}|'.format(row[0],  row[1], row[2],  number_of_episodes,  str(missing_episodes)[1:-1]))
-                print('-------------------------------------------------------------------------------------------------------------------------------------------------')
+                self.__log.debug('Missing episodes: '+str(missing_episodes)[1:-1])
+                unwatched_unfinished_shows.append('|{:43s}: | S{:2s} ({:2d}/{:2d})| missing: {:74s}|'.format(row[0],  row[1], row[2],  number_of_episodes,  str(missing_episodes)[1:-1]))
             else:
-                finished_tvshows.append('{:35s}: Season {:2s} and has {:2d}/{:2d} Episodes'.format( row[0],  row[1],  row[2],  number_of_episodes))
+                unwatched_finished_shows.append('{:35s}: Season {:2s} and has {:2d}/{:2d} Episodes'.format( row[0],  row[1],  row[2],  number_of_episodes))
                 
-        print('###############################################################')
-        print('######################## Watched Missing ######################')
-        print('###############################################################')
-        # Select TV-Shows where at least one Episode was played
-        cur.execute('select * from (select tvshow.c00 as Title, episodeview.c12 as Season, count(*) as Episodes, tvshow.c12 as SeriesiD, episodeview.idSeason as SeasoniD, sum(episodeview.playCount) as Played from episodeview join seasons on seasons.idSeason = episodeview.idSeason join tvshow on tvshow.idShow = seasons.idShow group by tvshow.c00, episodeview.c12 order by tvshow.c00) where Played is not NULL;')
-        rows = cur.fetchall()
-        finished_tvshows_watching = []
-        print('-------------------------------------------------------------------------------------------------------------------------------------------------')
-        print('|{:35s}({:8s})  | {:s} ({:s}/{:s})| {:74s}|'.format('Title', 'SeasonId', 'Season', 'Downloaded',  'Available',  'Missing'))
-        print('-------------------------------------------------------------------------------------------------------------------------------------------------')
-        for row in rows:
-            self.__log.debug('#####################################')
-            self.__log.debug('Currently Treating {:s}'.format(row[0]))
-            self.__log.debug('#####################################')
+
+        
+        for row in somewatched:
             if(int(row[1]) == 0): # Don't take into consideration Season 0
                 continue
-            self.__log.debug('Currently treating series {:s} with id: {:s}'.format(row[0],  row[3]))
-            show = self.__db.get(int(row[3]), "en" )
-            number_of_episodes = len(show[int(row[1])])
+            self.__log.debug('Currently treating series {:s} with id: {:s} and Season {:s}'.format(row[0],  row[3],  row[1]))
+            number_of_episodes = self.getTotalNumberOfEpisodes(int(row[3]),  int(row[1]))
             full_episodes = range(1, number_of_episodes+1)
             if(int(number_of_episodes) != int(row[2])): # If number of local Episodes is different from TheTVDB
                 cur.execute('select tvshow.c00 as Title, episodeview.c12 as Season, episodeview.c13 as Episode, tvshow.c12 as SeriesiD  from episodeview join seasons on seasons.idSeason = episodeview.idSeason join tvshow on tvshow.idShow = seasons.idShow where Season={:s} and SeriesiD={:s}  order by tvshow.c00, episodeview.c12, episodeview.c13;'.format(row[1],  row[3]))
@@ -129,25 +129,51 @@ class TVShows:
                 for episode in episodes:
                     present_episodes.append(episode[2])
                 present_episodes = map(int,  present_episodes)
-                self.__log.debug('Present episodes '+str(present_episodes))
+                self.__log.debug('Present episodes: '+str(present_episodes))
                 missing_episodes = list(set(full_episodes) - set(present_episodes))
-                self.__log.debug(str(missing_episodes)[1:-1])
-                print('|{:35s}({:8s}): | S{:2s} ({:2d}/{:2d})| missing: {:74s}|'.format(row[0], row[3], row[1], row[2],  number_of_episodes,  str(missing_episodes)[1:-1]))
-                print('-------------------------------------------------------------------------------------------------------------------------------------------------')
+                self.__log.debug('Missing episodes: '+str(missing_episodes)[1:-1])
+                watchedsome_unfinished_shows.append('|{:35s}({:8s}): | S{:2s} ({:2d}/{:2d})| missing: {:74s}|'.format(row[0], row[3], row[1], row[2],  number_of_episodes,  str(missing_episodes)[1:-1]))
             elif int(number_of_episodes) > row[5]:
-                finished_tvshows_watching.append('{:35s}: Season {:2s} and has watched {:2d}/{:2d} Episodes'.format( row[0],  row[1],  row[5],  number_of_episodes))
-                
+                watchedsome_finished_shows.append('{:35s}: Season {:2s} and has watched {:2d}/{:2d} Episodes'.format( row[0],  row[1],  row[5],  number_of_episodes))
+        con.close()
+        return unwatched_finished_shows,  unwatched_unfinished_shows,  watchedsome_unfinished_shows,  watchedsome_finished_shows
+        
+        
+            
+    def main(self):
+        unwatched_finished_shows,  unwatched_unfinished_shows,  watchedsome_unfinished_shows,  watchedsome_finished_shows = self.getSeriesInformation()
+        print('##############################################################')
+        print('###################### Unwatched Missing #####################')
+        print('##############################################################')
+        
+        print('-------------------------------------------------------------------------------------------------------------------------------------------------')
+        print('|{:44s} | {:s} ({:s}/{:s})| {:74s}|'.format('Title', 'Season', 'Downloaded',  'Available',  'Missing'))
+        print('-------------------------------------------------------------------------------------------------------------------------------------------------')
+        for row in unwatched_unfinished_shows:
+            print row
+            print('-------------------------------------------------------------------------------------------------------------------------------------------------')
+            
+        print('###############################################################')
+        print('######################## Watched Missing ######################')
+        print('###############################################################')
+        
+        print('-------------------------------------------------------------------------------------------------------------------------------------------------')
+        print('|{:35s}({:8s})  | {:s} ({:s}/{:s})| {:74s}|'.format('Title', 'SeasonId', 'Season', 'Downloaded',  'Available',  'Missing'))
+        print('-------------------------------------------------------------------------------------------------------------------------------------------------')
+        for row in watchedsome_unfinished_shows:
+            print row
+            print('-------------------------------------------------------------------------------------------------------------------------------------------------')
         
         print('###############################################################')
         print('######################## Ready to Watch #######################')
         print('###############################################################')
-        for finished in finished_tvshows:
+        for finished in unwatched_finished_shows:
             print(finished)
             
         print('###############################################################')
         print('#################### Complete and Watching ####################')
         print('###############################################################')
-        for finished in finished_tvshows_watching:
+        for finished in watchedsome_finished_shows:
             print(finished)
         
     def getArguments(self, argv):
