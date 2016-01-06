8790eef08728e0889d1f840e37378d4c57c49b9f
digsim <andreas.ruppen+github@gmail.com>
2016-01-06 17:11:21 +0100

    Raised version number for next deployment

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(fname):
 
 setup(
     name="missingTvShows",
-    version="1.1.5",
+    version="1.1.6",
     author="Andreas Ruppen",
     author_email="andreas.ruppen@gmail.com",
     description="Manages Kodi TVShows",
