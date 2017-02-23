c45dd3d4ae349c431ef6eff21fc5a522f0b1690e
digsim <andreas.ruppen+github@gmail.com>
2017-02-23 22:13:57 +0100

    Raised version number to 1.2.2 for next release

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def read(fname):
 
 setup(
     name="mtvs",
-    version="1.2.1.dev0",
+    version="1.2.2",
     author="Andreas Ruppen",
     author_email="andreas.ruppen@gmail.com",
     description="Manages Kodi TVShows",
