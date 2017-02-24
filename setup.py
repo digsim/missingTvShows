d27a01b55f259f2b362295fd25b79e0266796baf
digsim <andreas.ruppen+github@gmail.com>
2017-02-25 00:04:56 +0100

    New dev version

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def read(fname):
 
 setup(
     name="missingtvshows",
-    version="1.2.2",
+    version="1.2.2.dev0",
     author="Andreas Ruppen",
     author_email="andreas.ruppen@gmail.com",
     description="Manages Kodi TVShows",
