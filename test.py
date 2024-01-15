588c799883f1d54d18e270f3e87ad59f8cbc65de
Andreas Ruppen <andreas.ruppen@gmail.com>
2024-01-15 16:42:14 +0100

    Implement github actions

--- test.py
+++ test.py
@@ -6,12 +6,9 @@ from mtvs.main import MainImpl
 
 def main():
     """Entry point for the application script"""
-    a=5
     main = MainImpl()
     sys.argv = ['missingTvShow', 'sync']
-    print(sys.argv[1:])
     main.getArguments(sys.argv[1:])
-    #main.getArguments(['sync'])
 
 
 if __name__ == "__main__":
